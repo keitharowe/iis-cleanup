387bab3297990cdfec1eee27e7c7c15417064f72
Keith Rowe <krowe@aztekweb.com>
2021-02-17 10:03:25 -0500

    initial release of scripts; deleting of folders not yet implemented

---
+++ iis-hosting-folder-cleanup.ps1
@@ -0,0 +1,55 @@
+﻿Import-Module WebAdministration
+
+# Script to delete any ophaned folders assuming a "root" directory that all sites would start from.
+# any folders found in the "root" that do not have a cooresponding site, application, or virtual directory in IIS will be deleted
+# this initial version only outputs the folders to be deleted
+
+$rootPath = "e:\www"
+$WebsiteFolders = Get-ChildItem -Path $rootPath -Directory 
+$Websites = Get-ChildItem IIS:\Sites
+[bool] $found = $false
+
+foreach ($Folder in $WebsiteFolders) {
+    $writeFolder = $Folder.FullName
+    Write-Host("Working with folder: {0}" -f $writeFolder) 
+    if(Test-Path $Folder.FullName)
+    {
+    [String] $comparePath = ("{0}*" -f $Folder.FullName)
+    $SearchWebsites = $Websites | Where-Object { (($_.PhysicalPath -like $comparePath))} 
+    
+    if ($SearchWebsites.length -eq 0) {
+        $found = $false;
+        foreach($site in $Websites | Sort-Object Name )
+        {
+            $SearchVirtDir = Get-WebVirtualDirectory -Site $Site.name | Where-Object { (($_.PhysicalPath -like $comparePath))}   
+            if($searchVirtDir.length -gt 0)
+            {
+                $found = $true;
+            }
+            
+            if($found)
+            {
+                break;
+            }
+            $SearchAppDir = Get-WebApplication -Site $Site.name | Where-Object { (($_.PhysicalPath -like $comparePath))}   
+            if($SearchAppDir.length -gt 0)
+            {
+                $found = $true;
+            }
+            
+            if($found)
+            {
+                break;
+            }
+        }
+        if($found -eq $false)
+        {
+            Write-Host ">>>>> $writeFolder not found in IIS. Marked for deletion" -ForegroundColor yellow
+        }
+    }
+  }
+  else
+  {
+    Write-Host("Physical path ({0}) does not exist" -f $Folder.FullName) -ForegroundColor red
+  }
+}
---
+++ iis-hosting-site-cleanup.ps1
@@ -0,0 +1,35 @@
+﻿Import-Module WebAdministration
+
+# Script to delete all stopped sites, folders and application pools
+
+$Websites = Get-ChildItem IIS:\Sites | Where-Object {$_.state -eq “Stopped”} 
+foreach ($Site in $Websites) {
+  Write-Host("Working with IIS Site: {0}" -f $Site.name)
+
+  if(Test-Path $Site.physicalPath)
+  {
+    Write-Host("Deleting {0}" -f $Site.physicalPath)
+    Remove-Item $Site.PhysicalPath -Force -Recurse
+  }
+  else
+  {
+    Write-Host("Site physical path ({0}) does not exist" -f $Site.physicalPath)
+  }
+}
+$AppNames = ($Websites).name
+foreach($appName in $AppNames)
+    {
+        #Remove Site
+        if (Test-Path IIS:\Sites\$appName -PathType Container)
+            {
+                Write-Host("Deleting IIS Site: {0}" -f $appName)
+                Remove-Item IIS:\Sites\$appName -Force -Recurse
+            }
+        if (Test-Path IIS:\AppPools\$appName -PathType Container)
+            {
+                Write-Host("Deleting IIS Pool: {0}" -f $appName)
+                Remove-Item IIS:\AppPools\$appName -Force -Recurse
+            }
+    }
+
+   
